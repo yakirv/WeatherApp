2b8e7da596ac91389b8fc4ba947c29f296abbf91
Yakirv <yakirv@gmail.com>
2025-04-06 20:49:41 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { watchFile } = require("fs");
-const webpack = require("webpack");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { watchFile } = require('fs')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: "./src/index.js",
+    entry: './src/index.js',
 
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'docs'),
+        clean: true,
+    },
 
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  stats: {
-    children: true,
-  },
+    devtool: 'eval-source-map',
+    devServer: {
+        watchFiles: ['./src/template.html'],
+    },
+    stats: {
+        children: true,
+    },
 
-  mode: "development",
+    mode: 'development',
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: "html-loader",
-      },
-      {
-        test: /\.(png|jpg|jpeg)$/i,
-        type: "asset/resource",
-      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
     ],
-  },
-};
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
