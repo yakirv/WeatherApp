a03d5266cb8216ff2b78d34379eca1976a0daa88
Yakirv <yakirv@gmail.com>
2025-04-06 21:22:17 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles.css'
-
-import { ApiHandler } from './moduls/apiHandler'
-import { UI } from './moduls/ui'
-import { EventHandler } from './moduls/eventHandler'
-
-export const apiHandler = new ApiHandler()
-export const eventHandler = new EventHandler()
-export const ui = new UI()
